54a73e5ef30b5dfc8ccca65aa8c48542c5aaf3e0
Alexandr_TS <alexandr@tsaplin.ru>
2020-05-13 09:10:21 +0500

    Add Optional

---
+++ Solutions/Optional/optional.h
@@ -0,0 +1,141 @@
+// Исключение этого типа должно генерироваться при обращении к "пустому" Optional в функции Value
+struct BadOptionalAccess {
+};
+
+template <typename T>
+class Optional {
+private:
+    // alignas нужен для правильного выравнивания блока памяти
+    alignas(T) unsigned char data[sizeof(T)];
+    bool defined = false;
+
+public:
+    Optional() = default;
+    Optional(const T& elem) 
+        : defined(true) {
+		new (data) T(elem);
+    }
+
+    Optional(T&& elem) 
+        : defined(true) {
+        new (data) T(move(elem));
+    }
+
+    Optional(const Optional& other) 
+        : defined(other.HasValue()) {
+		if (other.HasValue()) {
+            new (data) T(other.Value());
+		}
+    }
+
+    Optional(Optional&& other) 
+        : defined(other.HasValue()) {
+		if (other.HasValue()) {
+            new (data) T(move(other.Value()));
+		}
+    }
+
+    Optional& operator=(const T& elem) {
+        if (defined) {
+            Value().~T();
+        }
+        else {
+            defined = true;
+        }
+        new (data) T(elem);
+        return *this;
+    }
+
+    Optional& operator=(T&& elem) {
+        if (defined) {
+            Value().~T();
+        }
+        else {
+            defined = true;
+        }
+        new (data) T(move(elem));
+        return *this;
+    }
+
+    Optional& operator=(const Optional& other) {
+        if (defined) {
+            if (other.HasValue()) {
+                Value() = other.Value();
+            }
+            else {
+                defined = other.HasValue();
+                Value().~T();
+            }
+        }
+        else {
+            defined = other.HasValue();
+            if (other.HasValue()) {
+                new (data) T(other.Value());
+            }
+        }
+        return *this;
+    }
+
+    Optional& operator=(Optional&& other) {
+        if (defined) {
+            if (other.HasValue()) {
+                Value() = move(other.Value());
+            }
+            else {
+                defined = other.HasValue();
+                Value().~T();
+            }
+        }
+        else {
+            defined = other.HasValue();
+            if (other.HasValue()) {
+                new (data) T(move(other.Value()));
+            }
+        }
+        return *this;
+    }
+
+    bool HasValue() const {
+        return defined;
+    }
+
+    // Эти операторы не должны делать никаких проверок на пустоту.
+    // Проверки остаются на совести программиста.
+    T& operator*() {
+        return *reinterpret_cast<T*>(data);
+    }
+    const T& operator*() const {
+        return *reinterpret_cast<T*>(data);
+    }
+    T* operator->() {
+        return reinterpret_cast<T*>(data);
+    }
+    const T* operator->() const {
+        return reinterpret_cast<T*>(data);
+    }
+
+    // Генерирует исключение BadOptionalAccess, если объекта нет
+    T& Value() {
+        if (!defined) {
+            throw BadOptionalAccess();
+        }
+        return *reinterpret_cast<T*>(data);
+    }
+    const T& Value() const {
+        if (!defined) {
+            throw BadOptionalAccess();
+        }
+        return *reinterpret_cast<const T*>(data);
+    }
+
+    void Reset() {
+        if (defined) {
+            Value().~T();
+            defined = false;
+        }
+    }
+
+    ~Optional() {
+        reinterpret_cast<T*>(data)->~T();
+    }
+};
